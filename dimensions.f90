765d9a857cf1fb788b1873f3ad15634e5a94dd65
blburkholder <blburkholder@alaska.edu>
2017-10-18 14:42:26 -0800

    first commit

---
+++ dimensions.f90
@@ -0,0 +1,10 @@
+module dimensions
+      implicit none
+      save
+
+      integer, parameter:: nx = 3, ny = 3, nz = 7001 !61,201
+
+      integer*4, parameter:: Ni_max = 400000  
+      
+end module dimensions
+      
